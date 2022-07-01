c3681cc49c2fb4a9a9f496120cfcbd259f4a3e45
Marcel Rieder <marieder22@gmail.com>
2022-07-01 12:08:12 +0200

    Improved abstraction via encapsulation. (Made methods called blinds_up and blinds_down)

--- pyStepper.py
+++ pyStepper.py
@@ -3,58 +3,77 @@ import time
 import sys
 
 
-GPIO.setmode(GPIO.BOARD)
+steps_all_the_way=7000
 
-DIR=33
-PUL=35
-ENA = 37
+def main():
+    blinds_up()
+    blinds_down()
 
-Sets=(DIR,PUL,ENA)
 
-DIR_Left=GPIO.HIGH
-DIR_Right=GPIO.LOW
+def blinds_up():
+    turn_x_steps(steps_all_the_way)    
 
-ENA_Locked = GPIO.LOW
-ENA_Released =GPIO.HIGH
 
-GPIO.setwarnings(False)
+def blinds_down():
+    turn_x_steps(other_direction(steps_all_the_way))
 
-GPIO.setup(Sets,GPIO.OUT)
 
-steps=int(sys.argv[1])
-direction = steps
+def other_direction(number):
+    return -1*number
 
-GPIO.output(ENA, ENA_Locked)
 
-if (steps<0):
-   GPIO.output(DIR,DIR_Right)
-else:
-    GPIO.output(DIR,DIR_Left)
 
-try:
+#Positive values pull blinds up, negative let it back down
+def turn_x_steps(steps):
 
- while(True):
-  if (steps<0):
-   GPIO.output(DIR,DIR_Right)
-  else:
-    GPIO.output(DIR,DIR_Left)
+    GPIO.setmode(GPIO.BOARD)
 
-  for i in range(abs(steps)):
-    GPIO.output(PUL,GPIO.HIGH)
-    time.sleep(0.0001875)
+    DIR=33
+    PUL=35
+    ENA = 37
 
-    GPIO.output(PUL,GPIO.LOW)
-    time.sleep(0.0001875)
- 
-  time.sleep(1)
- 
-  if (steps>0):
-   steps=~steps+1
-  else:
-   steps=~steps-1
+    Sets=(DIR,PUL,ENA)
 
-except KeyboardInterrupt:
-    
-  GPIO.output(ENA,ENA_Released)
-  print("The motor has been stopped")
+    DIR_Left=GPIO.HIGH
+    DIR_Right=GPIO.LOW
 
+    ENA_Locked = GPIO.LOW
+    ENA_Released =GPIO.HIGH
+
+    GPIO.setwarnings(False)
+
+    GPIO.setup(Sets,GPIO.OUT)
+
+    direction = steps
+
+    GPIO.output(ENA, ENA_Locked)
+
+    if (steps<0):
+       GPIO.output(DIR,DIR_Right)
+    else:
+        GPIO.output(DIR,DIR_Left)
+
+    try:
+
+        for i in range(abs(steps)):
+            GPIO.output(PUL,GPIO.HIGH)
+            time.sleep(0.0001875)
+
+            GPIO.output(PUL,GPIO.LOW)
+            time.sleep(0.0001875)
+
+        time.sleep(1)
+
+
+    except KeyboardInterrupt:
+        
+      GPIO.output(ENA,ENA_Released)
+      print("The motor has been stopped")
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
